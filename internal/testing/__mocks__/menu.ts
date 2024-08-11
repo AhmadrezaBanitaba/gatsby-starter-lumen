25a910f6ccb25b1dd461ba425cd13c0c610f1ff7
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2024-08-10 22:31:50 -0700

    Update menu.ts

--- internal/testing/__mocks__/menu.ts
+++ internal/testing/__mocks__/menu.ts
@@ -2,4 +2,5 @@ export default [
   { label: "Articles", path: "/" },
   { label: "About Me", path: "/pages/about" },
   { label: "Contact Me", path: "/pages/contacts" },
+  { label: "Dev", path: "/pages/dev" }
 ];
